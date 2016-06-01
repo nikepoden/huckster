7e95ee2d589363c612a614fc1ce4fe32d47a2653
Alexander <gofrokot@gmail.com>
2016-06-01 09:41:48 +0300

    added comment module

---
+++ client/jtest.js
@@ -0,0 +1,44 @@
+﻿/*function GetAvitoId(url) {
+            var id = url.match (/[^\?]*_(\d+)($|#.*|\?.*)/);
+//          console.log ("Url "+url);
+//          console.log (id);
+            if (!id)
+                return "";
+            return "avito:"+id[1];
+        };
+		
+*/
+document.addEventListener('DOMContentLoaded', function() {
+	var checkPageButton = document.getElementById('checkPage');
+	checkPageButton.addEventListener('click', function() {
+
+	var message = document.getElementById('message').value;
+	var author = document.getElementById('author').value;
+	
+	
+	/*var xxx = chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+		function(tabs){
+		return(tabs[0].url);
+   });*/
+   //alert(xxx);
+	
+	var data = 
+	{
+	"id_item": "1234", 
+	"message": message, 
+	"author": author
+	};
+	
+	$.ajax({
+		type : "POST",
+		url : "http://localhost:5000/comment/",
+		data: JSON.stringify(data, null, '\t'),
+		contentType: 'application/json;charset=UTF-8',
+		success: function(result) {
+			//window.alert(result);
+		}
+	});		
+  
+    window.alert("Ended")
+  }, false);
+}, false);
---
+++ modules/comment/__init__.py
@@ -0,0 +1,56 @@
+from moduleBase import ModuleBase
+from flask import abort, jsonify, request
+
+def GetModuleInstance():
+    return ModuleComment()
+
+comments = [
+    {
+        'id': 1,
+        'id_item': u'1234',
+        'author': u'Vasya',
+        'message': u'Message1'
+    },
+    {
+        'id': 2,
+        'id_item': u'1234',
+        'author': u'Sergey',
+        'message': u'Message2'
+    }
+]
+
+class ModuleComment(ModuleBase):
+    def __init__(self):
+        ModuleBase.__init__(self, "/comment/")
+        self.SetGet(self.execGet)
+        self.SetGetById(self.execGetById)
+        self.SetPost(self.execPost)
+        print "Loaded module: {0}".format(self)
+
+    def execGet(self):
+        return jsonify({'messages': comments})
+
+    def execGetById(self, id):
+        message = filter(lambda t: t['id'] == id, comments)
+        if len(message) == 0:
+            abort(404)
+        return jsonify({'messages': comments[0]})
+
+    def execPost(self):
+        print request.json
+        if not request.json: #or not 'title' in request.json:
+           abort(400)
+        comment = {
+            'id': comments[-1]['id'] + 1,
+            'id_item': request.json['id_item'],
+            'author': request.json.get('author', ""),
+            'message': request.json.get('message', "")
+        }
+        comments.append(comment);
+        return jsonify({'message': comment}), 201
+        #return "exec post from dynamic module"
+
+    def init(self):
+        self.SetGet(self.execPost)
+        self.SetGetById(self.execGetById)
+        self.SetPost(self.execPost)
